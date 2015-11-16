521271a7fff4ef4a53e6dcb0197f7153059b2ece
Jason English <Jason.English@mccann.com>
2015-11-15 19:57:31 -0500

    First commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name claritystormApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the claritystormApp
+ */
+angular.module('claritystormApp')
+    .controller('AboutCtrl', function ($scope) {
+        $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+    });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name claritystormApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the claritystormApp
+ */
+angular.module('claritystormApp')
+    .controller('MainCtrl', function ($scope) {
+        $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+    });
